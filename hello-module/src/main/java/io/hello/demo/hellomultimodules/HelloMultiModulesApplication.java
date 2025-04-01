e1ea5c2c16cf0fbeff6a0ece6f8c0d2a582242b3
lucky <lucky.s.bang@selvas.com>
2025-04-01 13:31:55 +0900

    init commit

---
+++ hello-module/src/main/java/io/hello/demo/hellomultimodules/HelloMultiModulesApplication.java
@@ -0,0 +1,13 @@
+package io.hello.demo.hellomultimodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMultiModulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMultiModulesApplication.class, args);
+    }
+
+}
---
+++ hello-module/src/test/java/io/hello/demo/hellomultimodules/HelloMultiModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hello.demo.hellomultimodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMultiModulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
