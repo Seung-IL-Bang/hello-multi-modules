3884d8fada19c55a2a73d3089a4fa7ffda0d64ce
Seungil <seungilbang@khu.ac.kr>
2025-04-02 22:13:45 +0900

    feat: remove SseService import from PaymentApiApplication

--- payment-api/src/main/java/io/hello/demo/paymentapi/PaymentApiApplication.java
+++ payment-api/src/main/java/io/hello/demo/paymentapi/PaymentApiApplication.java
@@ -1,13 +1,12 @@
 package io.hello.demo.paymentapi;
 
 import io.hello.demo.inventoryapi.InventoryApiConfig;
-import io.hello.demo.sseapi.SseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(value = {InventoryApiConfig.class, SseService.class})
+@Import(value = {InventoryApiConfig.class})
 public class PaymentApiApplication {
 
     public static void main(String[] args) {
