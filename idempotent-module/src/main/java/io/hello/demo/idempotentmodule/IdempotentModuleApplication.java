dd14fa2e68b4f8c6fe1008f48b9808ffa4424ca4
lucky <lucky.s.bang@selvas.com>
2025-04-14 08:52:20 +0900

    feat: add idempotent-module to settings.gradle

---
+++ idempotent-module/src/main/java/io/hello/demo/idempotentmodule/IdempotentModuleApplication.java
@@ -0,0 +1,13 @@
+package io.hello.demo.idempotentmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotentModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdempotentModuleApplication.class, args);
+    }
+
+}
