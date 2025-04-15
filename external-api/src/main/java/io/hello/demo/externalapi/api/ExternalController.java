0b5e575c57c3a41438512adb1a4661c5cfe9ba0e
lucky <lucky.s.bang@selvas.com>
2025-04-15 16:10:46 +0900

    feat: add endpoint to simulate external API call with customizable exception status

--- external-api/src/main/java/io/hello/demo/externalapi/api/ExternalController.java
+++ external-api/src/main/java/io/hello/demo/externalapi/api/ExternalController.java
@@ -4,10 +4,12 @@ import io.hello.demo.externalapi.support.error.CoreException;
 import io.hello.demo.externalapi.support.error.ErrorType;
 import io.hello.demo.externalapi.support.response.ApiResponse;
 import io.hello.demo.utilsmodule.ThreadUtils;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/external")
@@ -15,6 +17,7 @@ public class ExternalController {
 
     /**
      * Simulate an external API call with a delay
+     *
      * @param delay
      * @return
      */
@@ -29,6 +32,7 @@ public class ExternalController {
 
     /**
      * Simulate an external API call with a delay and an error response
+     *
      * @param delay
      * @return
      */
@@ -43,6 +47,7 @@ public class ExternalController {
 
     /**
      * Simulate an external API call with a delay and Throw an exception
+     *
      * @param delay
      * @return
      */
@@ -53,5 +58,19 @@ public class ExternalController {
             defaultValue = "0") long delay) {
         ThreadUtils.sleep(delay); // Simulate a delay
         throw new CoreException(ErrorType.DEFAULT_ERROR);
+    }
+
+    @GetMapping("/api/v1/exception/status")
+    public ApiResponse<?> getDelayApiWithExceptionStatus(
+            @RequestParam(
+                    name = "delay",
+                    required = false,
+                    defaultValue = "0") long delay,
+            @RequestParam(
+                    name = "code",
+                    required = false,
+                    defaultValue = "500") int code) {
+        ThreadUtils.sleep(delay); // Simulate a delay
+        throw new ResponseStatusException(HttpStatusCode.valueOf(code));
     }
 }
