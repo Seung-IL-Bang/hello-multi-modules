35b0ec41c6eb5b430ca1788e2531668207503bc4
lucky <lucky.s.bang@selvas.com>
2025-04-17 16:27:54 +0900

    feat: add task-scheduler-module to project settings

---
+++ task-scheduler-module/src/main/java/io/hello/demo/taskschedulermodule/TaskSchedulerModuleApplication.java
@@ -0,0 +1,13 @@
+package io.hello.demo.taskschedulermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSchedulerModuleApplication.class, args);
+    }
+
+}
